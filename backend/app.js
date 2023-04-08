b979b2475fd232f38a1ac16a1baf6d967d712b98
anurag8546 <2000520100019@ietlucknow.ac.in>
2023-04-08 17:24:07 +0530

    updated APP

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,21 @@ app.post("/register", async (req, res) => {
         res.send({ status: "Error" })
     }
 })
+// Your code
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+// Your code
+
+
 app.post("/login-user", async (req, res) => {
     const { email, password } = req.body;
 
